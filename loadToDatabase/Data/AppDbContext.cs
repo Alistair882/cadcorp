9b7012dd912c6e4325915cae8dc542ed67619d0b
Alistair <alistair.brock@outlook.com>
2025-05-27 17:22:28 +0100

    Save

--- loadToDatabase/Data/AppDbContext.cs
+++ loadToDatabase/Data/AppDbContext.cs
@@ -6,10 +6,6 @@ namespace loadToDatabase.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext()
-        {
-        }
-
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
